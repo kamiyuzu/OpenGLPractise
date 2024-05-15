bd352ae409764885d02eef060f5f40653e171432
kamiyuzu <alberto.rev.a@gmail.com>
2024-05-15 15:58:55 +0200

    Add melinoe obj into our program

--- src/GPO_04_entrega_ejer2.cpp
+++ src/GPO_04_entrega_ejer2.cpp
@@ -3,6 +3,7 @@ ATG, 2020
 ******************************************************************************/
 
 #include <GpO.h>
+#include <GpOObjectLoader.hpp>
 
 // TAMA�O y TITULO INICIAL de la VENTANA
 int ANCHO = 800, ALTO = 600;  // Tama�o inicial ventana
@@ -15,90 +16,75 @@ const char* prac = "OpenGL(GpO) Iluminacion";   // Nombre de la practica (aparec
 
 #define GLSL(src) "#version 330 core\n" #src
 
-//  PROGRAMA 1 (Implementacion por vertices de iluminacion difusa con luz lejana)
+// const char* vertex_prog = leer_codigo_de_fichero("../data/prog.vs");
+// const char* fragment_prog = leer_codigo_de_fichero("../data/prog.fs");
+
 const char* vertex_prog1 = GLSL(
 layout(location = 0) in vec3 pos;
-layout(location = 1) in vec3 normal;
-out float ilu;
-
-uniform vec3 campos;
-uniform mat4 M;
-uniform mat4 PV;
-uniform vec3 luz = vec3(1, 1, 0) / sqrt(2.0f);
-
-void main() {
-	gl_Position = PV*M*vec4(pos, 1);
-
-	vec3 scene_vertex_position = vec3(M*vec4(pos, 1));
-	vec3 v = normalize(scene_vertex_position - campos); // unit_vertex_to_cam
-
-	mat3 M_adj = mat3(transpose(inverse(M)));
-	vec3 n = M_adj * normal;
-
-	vec3 nn = normalize(n);
-	vec3 r = reflect(-luz,nn); // perfect_reflexion
-	float difusa = dot(luz,nn); if (difusa < 0) difusa = 0; 
-	float prod_esc_vertex_perf_reflex = dot(r,v); if (prod_esc_vertex_perf_reflex < 0) prod_esc_vertex_perf_reflex = 0; 
-	float especular = pow(prod_esc_vertex_perf_reflex,16);
-	ilu = (0.1 + 0.6*difusa + 0.3*especular);  //10% Ambiente + 60% difusa + 30 especular
+layout(location = 1) in vec2 uv;
+out vec2 UV;
+uniform mat4 MVP;
+void main(){
+	gl_Position = MVP*vec4(pos, 1);
+	UV = uv;
 }
 );
 
 const char* fragment_prog1 = GLSL(
-in float ilu;     // Entrada = iluminaci�n de vertices (interpolados en fragmentos)
-out vec3 col;  // Color fragmento
+in vec2 UV;
+out vec3 col;
+uniform sampler2D unit;
 void main()
 {
-	col = vec3(1, 1, 0.9);
-	col = col*ilu;
+	col= texture(unit, UV).rgb;
 }
 );
 
 //////////////////////////////////////////////////////////////////////////////////
 
 
-//  PROGRAMA 2 (aqui implementaremos version en fragmentos)
-const char* vertex_prog2 = GLSL(
-layout(location = 0) in vec3 pos;
-layout(location = 1) in vec3 normal;
-out vec3 n;
-out vec3 v;
+// //  PROGRAMA 2 (aqui implementaremos version en fragmentos)
+// const char* vertex_prog2 = GLSL(
+// layout(location = 0) in vec3 pos;
+// layout(location = 1) in vec3 normal;
+// out vec3 n;
+// out vec3 v;
 
-uniform vec3 campos;
-uniform mat4 M;
-uniform mat4 PV;
+// uniform vec3 campos;
+// uniform mat4 M;
+// uniform mat4 PV;
 
-void main() {
-	gl_Position = PV*M*vec4(pos, 1);
+// void main() {
+// 	gl_Position = PV*M*vec4(pos, 1);
 
-	vec3 scene_vertex_position = vec3(M*vec4(pos, 1));
-	v = scene_vertex_position - campos; // unit_vertex_to_cam
+// 	vec3 scene_vertex_position = vec3(M*vec4(pos, 1));
+// 	v = scene_vertex_position - campos; // unit_vertex_to_cam
 
-	mat3 M_adj = mat3(transpose(inverse(M)));
-	n = M_adj * normal;
-}
-);
+// 	mat3 M_adj = mat3(transpose(inverse(M)));
+// 	n = M_adj * normal;
+// }
+// );
 
-const char* fragment_prog2 = GLSL(
-in vec3 v;
-in vec3 n;
-uniform vec3 luz = vec3(1, 1, 0) / sqrt(2.0f);
-out vec3 col;  // Color fragmento
-void main()
-{
-	vec3 v_n = normalize(v); // unit_vertex_to_cam
-	vec3 nn = normalize(n);
-	vec3 r = reflect(-luz,nn); // perfect_reflexion
+// const char* fragment_prog2 = GLSL(
+// in vec3 v;
+// in vec3 n;
+// uniform vec3 luz = vec3(1, 1, 0) / sqrt(2.0f);
+// out vec3 col;  // Color fragmento
+// void main()
+// {
+// 	vec3 v_n = normalize(v); // unit_vertex_to_cam
+// 	vec3 nn = normalize(n);
+// 	vec3 r = reflect(-luz,nn); // perfect_reflexion
 
-	float difusa = dot(luz,nn); if (difusa < 0) difusa = 0; 
-	float prod_esc_vertex_perf_reflex = dot(r,v_n); if (prod_esc_vertex_perf_reflex < 0) prod_esc_vertex_perf_reflex = 0; 
-	float especular = pow(prod_esc_vertex_perf_reflex,16);
-	float ilu = (0.1 + 0.6*difusa + 0.3*especular);  //10% Ambiente + 60% difusa + 30 especular
+// 	float difusa = dot(luz,nn); if (difusa < 0) difusa = 0; 
+// 	float prod_esc_vertex_perf_reflex = dot(r,v_n); if (prod_esc_vertex_perf_reflex < 0) prod_esc_vertex_perf_reflex = 0; 
+// 	float especular = pow(prod_esc_vertex_perf_reflex,16);
+// 	float ilu = (0.1 + 0.6*difusa + 0.3*especular);  //10% Ambiente + 60% difusa + 30 especular
 
-	col = vec3(1, 1, 0.9);
-	col = col*ilu;
-}
-);
+// 	col = vec3(1, 1, 0.9);
+// 	col = col*ilu;
+// }
+// );
 
 ////////////////////////////////  FIN PROGRAMAS GPU (SHADERS) //////////////////////////////////
 
@@ -115,15 +101,15 @@ objeto modelo;
 void dibujar_indexado(objeto obj)
 {
   glBindVertexArray(obj.VAO);              // Activamos VAO asociado al objeto
-  glDrawElements(GL_TRIANGLES,obj.Ni,obj.tipo_indice,(void*)0);  // Dibujar (indexado)
+  glDrawArrays(GL_TRIANGLES, 0, obj.vertices.size());
   glBindVertexArray(0);
 }
 
 
 // Variables globales
-mat4 Proy,View,M;
-vec3 campos=vec3(0.0f,1.0f,4.0f);											
-vec3 target=vec3(0.0f,0.9f,0.0f);
+mat4 Proy,View,M, R, T, S;
+vec3 campos=vec3(0.0f,0.0f,2.0f);											
+vec3 target=vec3(0.0f,0.0f,0.0f);
 vec3 up = vec3(0, 1, 0);
 int prog_selected = 0;
 float az=0, el=0.75;
@@ -134,11 +120,12 @@ float az=0, el=0.75;
 void init_scene()
 {
 	prog[0] = Compile_Link_Shaders(vertex_prog1, fragment_prog1); // Compile shaders prog1
-	prog[1] = Compile_Link_Shaders(vertex_prog2, fragment_prog2); // Compile shaders prog2
+	// prog[1] = Compile_Link_Shaders(vertex_prog2, fragment_prog2); // Compile shaders prog2
 	
 	glUseProgram(prog[prog_selected]);
 
-	modelo = cargar_modelo((char*) "bin/data/esfera_520_n.bix");
+	modelo = cargar_modelo((char*) "bin/data/melinoe_weapons.obj");
+	GLuint tex0 = cargar_textura("bin/data/luna.jpg", GL_TEXTURE0);
 
 	Proy = glm::perspective(glm::radians(55.0f), 4.0f / 3.0f, 0.1f, 100.0f); 
 	View = glm::lookAt(campos,target,up);
@@ -159,11 +146,15 @@ void render_scene()
 
 	vec3 light_dir = vec3(cos(el)*cos(az), sin(el), cos(el)*sin(az));
 	vec3 xy=vec3(cos(tt), 1.0f, sin(tt));
-	M = translate(xy)*rotate(50*tt, vec3(0.0f, 1.0f, 0.0f));   // Mov modelo 
+	S = scale(mat4(1.0f),vec3(2.0f));
+    R = rotate(90.0f, vec3(0.0f, 0.0f, 1.0f))*rotate(30.0f, vec3(1.0f, 0.0f, 0.0f))*rotate(120.0f, vec3(0.0f, 1.0f, 0.0f));
+    // T = translate(glm::vec3(2.5*cos(t), 2.5*sin(t), 0));   	
+	M = R * S;
+	// M = rotate(90.0f, vec3(0.0f, 0.0f, 1.0f))*rotate(90.0f, vec3(0.0f, 1.0f, 0.0f));   // Mov modelo 
 	
-	transfer_mat4("PV",Proy*View); transfer_mat4("M", M);
-	transfer_vec3("luz", light_dir);
-	transfer_vec3("campos", campos);
+	transfer_mat4("MVP",Proy*View*M);
+	// transfer_vec3("luz", light_dir);
+	// transfer_vec3("campos", campos);
 	dibujar_indexado(modelo);
 	
 }
