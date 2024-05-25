c5ce15b0bb00498bde313d84f5a1f63b2286416e
kamiyuzu <alberto.rev.a@gmail.com>
2024-05-25 16:39:49 +0200

    Add camera movement into toon shading

--- src/GPO_04_entrega_ejer3.cpp
+++ src/GPO_04_entrega_ejer3.cpp
@@ -62,18 +62,18 @@ void main()
 	float difusa = dot(luz,nn); if (difusa < 0) difusa = 0; 
 	float ilu = (ilu_coef.x + ilu_coef.y*difusa + ilu_coef.z*spec);
 
-	float edge_thresh=0.0f;
+	float edge_thresh=0.4f;
 	float edge_detection = (ilu > edge_thresh) ? 0 : 1;
 
 	if(edge_detection == 0){
 		if (ilu > outter)
 			col = vec3(1.0,1.0,1.0);
 		else if (ilu > mid)
-			col = vec3(0.6,0.3,0.3);
+			col = vec3(0.8,0.4,0.4);
 		else if (ilu > lower)
-			col = vec3(0.4,0.2,0.2);
+			col = vec3(0.6,0.3,0.3);
 		else
-			col = vec3(0.2,0.1,0.1);
+			col = vec3(0.4,0.2,0.2);
 	} else {
 		float scale_origin = 0.9;
 		float scale = scale_origin+edge_thresh;
@@ -112,8 +112,8 @@ vec3 target=vec3(0.0f,0.0f,0.0f);
 vec3 up = vec3(0.0f, 1.0f, 0.0f);
 vec4 ilu_coef[3] = {vec4(0.2, 0, 0, 10), vec4(0, 1, 0, 40), vec4(0, 0, 1, 60)};
 int prog_selected = 0;
-float az=7.2f, el=3.16;
-float outter = 0.75f, mid = 0.5f, lower = 0.25f;
+float az=1.14f, el=1.4f, d = 1.2f, zfar = 25.0f, znear = 0.2f;
+float outter = 0.96f, mid = 0.85f, lower = 0.61f;
 
 // Compilaci�n programas a ejecutar en la tarjeta gr�fica:  vertex shader, fragment shaders
 // Preparaci�n de los datos de los objetos a dibujar, envialarlos a la GPU
@@ -126,9 +126,6 @@ void init_scene()
 
 	modelo = cargar_obj((char*) "bin/data/melinoeweapons.obj");
 	tex0 = cargar_textura("bin/data/melinoe.jpg", GL_TEXTURE0);
-
-	Proy = glm::perspective(glm::radians(55.0f), 4.0f / 3.0f, 0.1f, 100.0f); 
-	View = glm::lookAt(campos,target,up);
 
 	glEnable(GL_CULL_FACE); glEnable(GL_DEPTH_TEST);
 }
@@ -144,10 +141,14 @@ void render_scene()
 	
 	glUseProgram(prog[prog_selected]);
 
+	campos = d * vec3(sin(az)*cos(el), cos(az)*cos(el), sin(el));
 	vec3 light_dir = vec3(cos(el)*cos(az), sin(el), cos(el)*sin(az));
 	S = scale(mat4(1.0f),vec3(2.0f));
     R = rotate(95.0f, vec3(0.0f, 0.0f, 1.0f))*rotate(90.0f, vec3(1.0f, 0.0f, 0.0f))*rotate(120.0f, vec3(0.0f, 1.0f, 0.0f));
 	M = R * S;
+
+	Proy = perspective(radians(55.0f), 4.0f / 3.0f, znear, zfar); 
+	View = lookAt(campos,target,up);
 	
 	transfer_mat4("PV",Proy*View); transfer_mat4("M", M);
 	transfer_vec3("luz", light_dir);
@@ -238,13 +239,67 @@ static void KeyCallback(GLFWwindow* window, int key, int code, int action, int m
 		case GLFW_KEY_5: if(action) lower-=0.01f; break;
 		case GLFW_KEY_6: if(action) lower+=0.01f; break;				
 	}
+    fprintf(stdout,"el %f az %f\n", el, az);
+	fprintf(stdout,"outter %f mid %f lower %f\n", outter, mid, lower);
 }
 
+double xp, yp;
+
+static void moverCallback(GLFWwindow* window, double x, double y) {
+    // Los parámetros x, y nos dan la posición actual del cursor en la ventana.
+    fprintf(stdout,"x %.1f y %.1f\n", x, y);
+    double F = 0.007f * d;
+    double dx = x - xp;
+    double dy = y - yp;
+    az = F * dx;
+    el = F * dy;
+}
+
+GLfloat zp;
+
+void pulsarCallback(GLFWwindow* window, int Button, int Action, int Mode) {
+    // El parámetro Button nos da el botón pulsado (0=izquierdo, 1= derecho). 
+    // Action nos indica si se ha pulsado (1) o liberado (0). 
+    // Mode si se ha pulsado en combinación con alguna otra tecla (Shift, Ctrl, …).
+	fprintf(stdout, "Button %d Act %d Mode %d\n", Button, Action, Mode);
+    if (!Button && Action) {
+        glfwGetCursorPos(window,&xp,&yp);
+        glfwSetCursorPosCallback(window, moverCallback);
+    }
+
+    if (!Button && !Action) glfwSetCursorPosCallback(window, NULL);
+
+    if (Button && Action) {
+        glfwGetCursorPos(window,&xp,&yp);
+        glReadPixels((GLint) xp, (GLint) yp, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &zp);
+        fprintf(stdout, "xp= %.0f yp= %.0f zp= %.3f\n", xp, yp, zp);
+        float xn=(2*(xp/ANCHO))-1;
+        float yn=(2*(yp/ALTO))-1;
+        float zn=2*zp-1;
+        fprintf(stdout, "xn= %.0f yn= %.0f zn= %.3f\n", xn, yn, zn);
+        float z_cam = (-2*znear*zfar)/((zfar+znear)+zn*(znear-zfar));
+        float x_cam = -((xn*z_cam)/Proy[0][0]); 
+        float y_cam = -((yn*z_cam)/Proy[1][1]);
+        fprintf(stdout, "xcam= %.0f ycam= %.0f zcam= %.3f\n", x_cam, y_cam, z_cam);
+    }
+}
+
+static void scrollCallback(GLFWwindow* window, double dx, double dy) {
+    // Los parámetros dx,dy nos dan el "scrolling" en los ejes X e Y. 
+    // En el caso de usar la rueda del ratón dx=0 y solo tenemos "scrolling" vertical.
+    fprintf(stdout,"x %.1f y %.1f\n", dx, dy);
+    if (dy > 0.0f) d+=0.1f;
+    if (dy < 0.0f) d-=0.1f;
+    fprintf(stdout,"x %.1f y %.1f z %.1f\n", campos.x, campos.y, campos.z);
+    fprintf(stdout,"d %.1f\n", d);
+}
 
 void asigna_funciones_callback(GLFWwindow* window)
 {
 	glfwSetWindowSizeCallback(window, ResizeCallback);
 	glfwSetKeyCallback(window, KeyCallback);
+    glfwSetMouseButtonCallback(window, pulsarCallback);
+    glfwSetScrollCallback(window, scrollCallback);
 }
 
 
